439ff844f5bab8d872b8f9d2a6c294d270ec128a
Alex <alexander.j.myers@icloud.com>
2018-03-18 22:16:28 -0700

    fade led and speaker noises

--- sketch_mar17a.ino
+++ sketch_mar17a.ino
@@ -1,4 +1,5 @@
 #include <bluefruit.h>
+#include "pitches.h"
 
 BLEUart bleuart;
 
@@ -6,17 +7,27 @@ BLEUart bleuart;
 uint8_t readPacket (BLEUart *ble_uart, uint16_t timeout);
 float   parsefloat (uint8_t *buffer);
 void    printHex   (const uint8_t * data, const uint32_t numBytes);
+
 int led = 9;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 // Packet buffer
 extern uint8_t packetbuffer[];
-#define testLED   15
+
+#define LED         15
+#define SPEAKER     12
+#define VIB_MOTOR   30
+
+int melody[]= {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+int noteDurations[]={
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+int arrayLen = sizeof(melody);
+
 void setup(void)
 {
-  Serial.begin(115200);
-  Serial.println(F("Adafruit Bluefruit52 Controller App Example"));
-  Serial.println(F("-------------------------------------------"));
 
   Bluefruit.begin();
   Bluefruit.setName("Bluefruit52");
@@ -35,65 +46,72 @@ void setupAdv(void)
 {
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
-
-  // Include the BLE UART (AKA 'NUS') 128-bit UUID
   Bluefruit.Advertising.addService(bleuart);
-
-  // There is no room for 'Name' in the Advertising packet
-  // Use the optional secondary Scan Response packet for 'Name' instead
   Bluefruit.ScanResponse.addName();
 
-  pinMode(testLED, OUTPUT);
-  digitalWrite(testLED, LOW);
+  pinMode(LED, OUTPUT);
+  pinMode(VIB_MOTOR, OUTPUT);
+  pinMode(SPEAKER, OUTPUT);
+  digitalWrite(LED, LOW);
+  digitalWrite(VIB_MOTOR, LOW);
 }
 
-/**************************************************************************/
-/*!
-    @brief  Constantly poll for new command or response data
-*/
-/**************************************************************************/
+
 void loop(void)
 {
   // Wait for new data to arrive
   uint8_t len = readPacket(&bleuart, 500);
   if (len == 0) return;
 
-  // Buttons
   if (packetbuffer[1] == 'B') {
     uint8_t buttnum = packetbuffer[2] - '0';
     boolean pressed = packetbuffer[3] - '0';
-    Serial.print ("Button "); Serial.print(buttnum);
+
     if (pressed) {
       switch (buttnum) {
         case 1:
+          // makes the light blink fast
           for (int i = 0; i < 8; i++) {
-            digitalWrite(testLED, HIGH);
-            delay(300);
-            digitalWrite(testLED, LOW);
-            delay(300);
+            digitalWrite(LED, HIGH);
+            delay(100);
+            digitalWrite(LED, LOW);
+            delay(100);
           }
           break;
+
         case 2:
-          for (int k = 0; k >= 4; k+1) {
+          // make the light blink fade in-out
+          for (int k = 0; k >= 4; k + 1) {
             fade();
           }
           break;
+
         case 3:
-          digitalWrite(LED_BUILTIN, HIGH);
-          delay(3000);
-          digitalWrite(LED_BUILTIN, LOW);
+          // makes the speaker play noise
+          for (int thisNote =0; thisNote < arrayLen; thisNote++){
+            int dur = 1000/noteDurations[thisNote];
+            tone(SPEAKER, melody[thisNote];
+            int pause = dur * 1.30;
+            delay(pause);
+            noTone(SPEAKER);
+          }
           break;
+
         case 4:
-          digitalWrite(LED_BUILTIN, HIGH);
-          delay(4000);
-          digitalWrite(LED_BUILTIN, LOW);
+          // makes the vibration motor go
+          for (int i = 0; i <= 10; i++) {
+            analogWrite(VIB_MOTOR, 255);
+            delay(100);
+            analogWrite(VIB_MOTOR, 0);
+            delay(100);
+          }
           break;
       }
     }
   }
 }
 void fade() {
-  analogWrite(testLED, brightness);
+  analogWrite(LED, brightness);
   brightness = brightness + fadeAmount;
   if (brightness <= 0 || brightness >= 255) {
     fadeAmount = -fadeAmount;
